466cf9958b557ad4f01ca8341b3108fa85ebb7fb
Alexis Mongin <alexis.mongin@contentsquare.com>
2022-04-28 15:00:14 +0200

    ff

--- main.py
+++ main.py
@@ -20,36 +20,11 @@ def home() -> str:
     return "pong"
 
 
-@app.get(
-    "/orderTest",
-    response_model=OrderResponse,
-    summary="ping",
-)
-def order_route(order: Order) -> OrderResponse:
-    """
-    Route to ping server for testing purposes
-    """
-
-    total_ht = 0.
-    for price, qnt in zip(order.prices, order.quantities):
-        total_ht +=  price * quantity
-
-    total = total_ht * (1. + TAX[order.country])
-
-    reduc = REDUCTIONS[order.reduction]
-    selec = None
-    for key, value in reduc.items():
-        if total > key:
-            selec = value
-
-    final = total * (1. - selec)
-
-    return {"total": final}
-
 @app.post(
     "/order",
     response_model=OrderResponse,
-    summary="ping"
+    summary="ping",
+    status_code=404
 )
 def order_route(order: Order) -> OrderResponse:
     """
