29cd22bcd1cba51f37126ce8151bea80a6e978c3
Slim Frikha <slim.frikha@contentsquare.com>
2022-04-28 14:30:31 +0200

    pydantic order shcema

--- schema.py
+++ schema.py
@@ -1,1 +1,13 @@
 from pydantic import BaseModel, Field
+from typing_extensions import Literal
+
+from data import TAX, REDUCTIONS
+
+countries_codes = list(TAX.keys())
+reduction_types = list(REDUCTIONS.keys())
+
+class Order(BaseModel):
+    prices: list[float] = Field(..., title="prices list")
+    quantities: list[int] = Field(..., title="quantities list")
+    country: Literal[countries_codes] = Field(..., title="country")
+    reduction: Literal[reduction_types] = Field(..., title="reduction")
